71813a4f5f88bd6748877d6ed58a4e90445b9080
Käpölä Kai <kai.kapola@iki.fi>
2021-02-03 16:17:35 +0200

    Quick and dirty push!

---
+++ map_test.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# Maps test for Vanitate
+#
+# Copyright (C) 2021 Kai Käpölä
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import yaml
+import networkx as nx
+import sys
+import logging as log
+
+
+class Ship:
+    """Ship functions"""
+    ships = []
+
+    def __init__(self):
+        pass
+
+    def add(self, name):
+        if not name in self.ships:
+            self.ships.append(name)
+            print("Ship", name, "added")
+        else:
+            print("EXIT: Duplicate ship name", name)
+            sys.exit
+
+    def __str__(self):
+        text = "SHIP INFO\n\tTotal ships: %s\n\tShips:" % (len(self.ships))
+        for s in self.ships:
+            text += "\n\t\t%s" % (s)
+        return(text)
+
+    def delete(self, name):
+        if not name in self.ships:
+            self.ships.remove(name)
+            print("Ship", name, "deleted")
+
+
+class SpaceMap:
+    """Space map functions"""
+
+    def __init__(self):
+        self.planet_types = []
+        self.map = nx.Graph()
+
+    def __str__(self):
+        text = "MAP INFO\n"
+        for n in nx.info(self.map).splitlines():
+            text += "\t%s\n" % (n)
+
+        text += "\tIsolated nodes: %s\n" % (len(list(nx.isolates(self.map))))
+
+        text += "\tNodes:\n\t\t"
+        for n in self.map.nodes():
+            text += "%s " % (n)
+
+        text += "\n\tEdges:\n\t\t"
+        for n in self.map.edges():
+            e = ",".join(n)
+            text += "(%s) " % (e)
+
+        if len(list(nx.isolates(self.map))) > 0:
+            text += "\n\tIsolated nodes:\n\t\t"
+            for n in list(nx.isolates(self.map)):
+                text += "%s " % (n)
+
+        return(text)
+
+    def read_space_map(self):
+        with open("./resources/map.yaml", 'r') as stream:
+            try:
+                yaml_data = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        for ss in yaml_data['solar systems'].keys():
+            ss_id = ss
+            ss_name = yaml_data['solar systems'][ss]['name']
+            ss_neighbors = yaml_data['solar systems'][ss]['neighbors']
+            self.add_solar_system(ss_id, ss_name, ss_neighbors)
+
+    def add_solar_system(self, id, name, neighbors):
+        self.map.add_node(id, name=name)
+
+        if neighbors != [None]:
+            for n in neighbors:
+                self.map.add_edge(id, n)
+
+    def write_networkx_map(self):
+        print("WRITING NX MAP")
+        nx.write_yaml(self.map, "./resources/nx_map.yaml")
+
+    def read_networkx_map(self):
+        print("READING NX MAP")
+        self.map = nx.read_yaml("./resources/nx_map.yaml")
+
+
+def main():
+
+    log.basicConfig(format='%(asctime)s:FILE %(filename)s:FUNCTION %(funcName)s:ROW %(lineno)d:%(levelname)s:%(message)s', filename='./log/main.log', level=log.DEBUG)
+    log.debug('This message should go to the log file')
+    log.info('So should this')
+    log.warning('And this, too')
+    log.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+    map = SpaceMap()
+
+    map.add_solar_system("test","Jee","")
+    map.read_space_map()
+    map.write_networkx_map()
+    map.read_networkx_map()
+    print(map)
+
+    ships = Ship()
+    ships.add("Mega1")
+    ships.add("Mega2")
+    ships.add("Mega3")
+
+    print(ships)
+
+
+
+if __name__ == "__main__":
+    main()
