2dab173fc21a9638618472b6a8fcbd815b347a79
Käpölä Kai <kai.kapola@iki.fi>
2021-02-05 21:17:00 +0200

    Quick and dirty push!

--- simulation_test.py
+++ simulation_test.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Time-stamp: <2021-02-05 14:14:50>
+# Time-stamp: <2021-02-05 14:16:55>
 
 # Standard libraries
 import sys
