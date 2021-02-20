a074da8029bdc5b4e85815e6527cb3dcbf715937
Käpölä Kai <kai.kapola@iki.fi>
2021-02-20 23:21:23 +0200

    Test

--- simulation_test.py
+++ simulation_test.py
@@ -772,7 +772,7 @@ def main():
                 sc_1 = space.scale_coordinates(
                     mouse_pos_1, [space.view_x_max, space.view_y_max], [space.view_x_min, space.view_y_min])
                 sc_2 = space.scale_coordinates(
-                    mouse_pos_2, [space.view_x_min, space.view_y_min], [space.view_x_max, space.view_y_max])
+                    mouse_pos_2, [space.view_x_max, space.view_y_max], [space.view_x_min, space.view_y_min])
 
                 log.info("Space coordinates: %s %s" % (sc_1, sc_2))
 
