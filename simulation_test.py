54671f8e857bf202e95a9678cfe4268be1fdc613
Käpölä Kai <kai.kapola@iki.fi>
2021-02-21 16:47:51 +0200

    Test

--- simulation_test.py
+++ simulation_test.py
@@ -634,7 +634,7 @@ class SpaceMapGenerator():
     def generate_planets(self, system):
         log.debug("Generating planets for system %s" % system)
 
-        planets = random.randrange(1, 1)
+        planets = random.randrange(1, 2)
 
         orbit_min = 57950000 + random.randrange(-10000000, 10000000)
         ormit_max = 5913000000 + random.randrange(-10000000, 10000000)
