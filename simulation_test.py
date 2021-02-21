ae8f1c0440ca54912a87afa39b2804da27549ec3
Käpölä Kai <kai.kapola@iki.fi>
2021-02-21 23:18:54 +0200

    Test

--- simulation_test.py
+++ simulation_test.py
@@ -735,9 +735,9 @@ def main():
     log.debug("pygame dislay modes: %s", pygame.display.list_modes())
     log.debug("Initializing pygame fonts")
 
-    pygame_font_size_16 = pygame.font.SysFont("menlo", 12)
-    pygame_font_size_22 = pygame.font.SysFont("menlo", 16)
-    pygame_font_size_32 = pygame.font.SysFont("menlo", 22)
+    pygame_font_size_16 = pygame.font.SysFont(None, 12)
+    pygame_font_size_22 = pygame.font.SysFont(None, 16)
+    pygame_font_size_32 = pygame.font.SysFont(None, 22)
     ships.font = pygame_font_size_22
     space.font_size_s = pygame_font_size_16
     space.font_size_m = pygame_font_size_22
