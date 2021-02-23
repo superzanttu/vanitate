84bbc132c39ee45853cea052daf77eaaaf4162ad
Käpölä Kai <kai.kapola@iki.fi>
2021-02-24 00:22:57 +0200

    Quick and dirty!

--- simulation_test.py
+++ simulation_test.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Time-stamp: <2021-02-23 00:19:49>
+# Time-stamp: <2021-02-23 00:20:05>
 import logging
 import sys
 import math
@@ -734,7 +734,7 @@ class StarSprite(pygame.sprite.Sprite):
 
         # Call the parent class (Sprite) constructor
         # super().__init__()
-        # pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
         # super().__init__()
         log.info("__init__")
 
