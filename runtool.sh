cf29ddf48c67e412f6812f10323cbcf098c750f2
Käpölä Kai <kai.kapola@iki.fi>
2021-02-23 22:31:13 +0200

    Run

--- runtool.sh
+++ runtool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Time-stamp: <2021-02-22 22:30:52>
+# Time-stamp: <2021-02-22 22:31:02>
 
 while :
 do
@@ -25,7 +25,7 @@ do
     r* )
       echo === Run start.sh
       git add -v .
-      git diff 
+      git diff -v
       git commit -m "Run"
       time ./start.sh
       ;;
