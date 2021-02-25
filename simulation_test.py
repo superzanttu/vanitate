5b0e6530c70ed3caad28fd5e5b789aee27d64c83
Käpölä Kai <kai.kapola@iki.fi>
2021-02-25 23:35:50 +0200

    Run

--- simulation_test.py
+++ simulation_test.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Time-stamp: <2021-02-24 21:32:26>
+# Time-stamp: <2021-02-24 21:35:49>
 import logging
 import sys
 import math
@@ -407,7 +407,7 @@ class Universe:
     universe_y_min = 0
     universe_y_max = 0
 
-    systems = AllSystems()
+    systems = self.AllSystems()
 
     class AllSystems:
         log.info("Class created")
