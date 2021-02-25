b591ae1005c10bc2fbf9c6bf49c062b4ad541c9c
Käpölä Kai <kai.kapola@iki.fi>
2021-02-25 23:53:58 +0200

    Run

--- simulation_test.py
+++ simulation_test.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Time-stamp: <2021-02-24 21:52:48>
+# Time-stamp: <2021-02-24 21:53:57>
 import logging
 import sys
 import math
@@ -401,7 +401,7 @@ Universe
 class Universe:
     log.info("Class created")
 
-    space = Space()
+    space = self.Space()
 
     def __init__(self):
         log.info("Universe")
