683e848b80219958963594cab4b60dabbf365441
Käpölä Kai <kai.kapola@iki.fi>
2021-02-26 00:38:07 +0200

    Run

--- simulation_test.py
+++ simulation_test.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Time-stamp: <2021-02-24 22:37:11>
+# Time-stamp: <2021-02-24 22:38:06>
 import logging
 import sys
 import math
@@ -449,6 +449,9 @@ class Universe:
 
                 new_system_name = "Suomi"
                 # Select name for system
+
+                print(self.__dict__)
+
                 while new_system_name in self.system_names:
                     new_system_name = self.markov.gen_name("finnish", 4, 13)
 
